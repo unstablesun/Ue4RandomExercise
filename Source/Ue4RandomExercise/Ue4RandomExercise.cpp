507f9ee13e35d0d74304389818d648fe9f40352b
Dave Hards <unstablesun@gmail.com>
2020-11-27 13:49:17 -0400

    blank ue4 project

---
+++ Source/Ue4RandomExercise/Ue4RandomExercise.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Ue4RandomExercise.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Ue4RandomExercise, "Ue4RandomExercise" );
---
+++ Source/Ue4RandomExercise/Ue4RandomExerciseGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Ue4RandomExerciseGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4RANDOMEXERCISE_API AUe4RandomExerciseGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
